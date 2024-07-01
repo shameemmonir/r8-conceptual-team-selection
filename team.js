e3fb8bbfd7e042e8fb257ee555741a3b524fa406
shameemmonir <shameemmonir@gmail.com>
2024-07-01 16:50:56 +0600

    third commit

--- team.js
+++ team.js
@@ -1,18 +1,21 @@
 // const price = document.getElementById('budget').innerText;
+// // console.log(typeof price);
 // const convertPrice = parseInt(price);
 
 // const cartCount = document.getElementById('cart').innerText;
-// const convertCartCount = parseInt(cartCount);
+// // console.log(typeof price);
+// const convertCartCount = parseInt(price);
 
 // const leftCount = document.getElementById('left').innerText;
-// const convertLeftCount = parseInt(leftCount);
+// // console.log(typeof price);
+// const convertLeftCount = parseInt(price);
+
 
 const allBtn = document.getElementsByClassName('add-btn');
 // console.log(allBtn);
-
-for(const btn of allBtn){
+for (const btn of allBtn) {
     // console.log(btn);
-    btn.addEventListener('click', function (event){
+    btn.addEventListener('click', function (event) {
         const name = event.target.parentNode.childNodes[1].innerText;
         const price = event.target.parentNode.childNodes[3].childNodes[1].innerText;
         const category = event.target.parentNode.childNodes[5].childNodes[1].innerText;
@@ -37,13 +40,10 @@ for(const btn of allBtn){
         selectedContainer.appendChild(div);
 
         updatedTotalCost(price);
-
-        // console.log(event.target.parentNode.childNodes[5].childNodes[1].innerText);
-        // console.log(event.target.parentNode.childNodes[3].childNodes[1].innerText);
     })
 }
 
-function updatedTotalCost (value){
+function updatedTotalCost(value) {
     // console.log(value);
     const totalCost = getConvertedValue('total-cost');
     const sum = totalCost + parseInt(value);
@@ -51,9 +51,12 @@ function updatedTotalCost (value){
 }
 
 
-function getConvertedValue (id){
+
+function getConvertedValue(id) {
     const price = document.getElementById(id).innerText;
+    // console.log(typeof price);
     const convertPrice = parseInt(price);
+    // console.log(typeof convertPrice);
     return convertPrice;
 }
 
